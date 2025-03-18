13e6871880d49edf966282fc7880125cbe36b7f8
gaurav <gauravkuntal393@gmail.com>
2025-03-18 14:00:02 +0530

    changes

---
+++ Array/prob_3.js
@@ -0,0 +1,67 @@
+ //question no-1
+ 
+ for(i=0;i<arr.length;i++){
+    console.log(hello)
+ }
+ ///time complexity =o(n)
+
+//question no-2
+for(i=0;i<arr.length;i+=2){
+    console.log(hello)
+}
+//time complexity =0(n)
+
+//question no-3
+let a = [1,2,3,4,], b = [2,3,4,1,2];
+let n = a.length
+let m = b.length
+for(let i=0;i<n;i++){
+    a[i]=i;
+}
+for(let i=0;i<m;i++){
+    b[i]=m-i;
+}
+console.log(hello)
+
+//time compexity=0(n+m)=O(n)
+
+//question no-4
+let arr = [2,3,4,5,65];
+let arr1 = [3,4,1,3,7];
+let n1 = arr.length;
+let m1 = arr1.length
+for(let i = 0; i < n; i++) {
+    for(let j = 0; j < m; j++) {
+        console.log("okay")
+    }
+}
+
+//time complexity= 0(n*n)
+
+//question no-5
+let c = 0;
+for(let i = 1; i < n; i *= k) {
+    c++;
+}
+//time complexity=o(n )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
