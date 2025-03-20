95e1d1c1b083088e346980e39dc8d19835e77db6
gaurav <gauravkuntal393@gmail.com>
2025-03-20 07:59:52 +0530

    prob5

--- Array/prob-4.js
+++ Array/prob-4.js
@@ -5,3 +5,27 @@ for(let  i = 0; i < n; i++) {
     }
 }
 //time complexity = o(nm)
+
+
+letc = 0;
+for( let i = 0; i < n; i++) {
+    for(let j = i+1; j < m; j++) {
+        c++;
+    }
+}
+//time complexity = o(nm)
+
+let c = 0;
+for(let i = 1; i <= n; i *= k) {
+    c++;
+}
+
+//time complexity = o(n)
+
+for(let i = 0; i < n; i++) {
+    for(let j = 0; j < m; j++) {
+        cout << "okay";
+    }
+}
+//time complexity =o(nm)
+
