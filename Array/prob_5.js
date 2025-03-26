3b7dc994ba493d3efb2fccff93a416d7777b8a93
gaurav <gauravkuntal393@gmail.com>
2025-03-26 11:34:58 +0530

    matrix

--- Array/prob_5.js
+++ Array/prob_5.js
@@ -12,9 +12,27 @@
 // }
 
 
+// let matrix=[
+//     [1,2,3],
+//     [4,5,6],
+//     [7,8,9]
+// ]
+// console.log(matrix[2][1])
+
+
 let matrix=[
-    [1,2,3],
+    [1,2,3],      
     [4,5,6],
     [7,8,9]
 ]
-console.log(matrix[2][1])
+let target=5
+let  result=[]
+// sum=0
+for(i=0;i<matrix.length;i++){
+for(j=0;j<matrix[i].length;j++){
+  if(matrix[i][j]==target){
+      result.push(i,j)
+  }
+}
+}
+console.log(result)
