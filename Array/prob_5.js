4c4cb914d56298fd8baa48f4e287a663aca3d927
gaurav <gauravkuntal393@gmail.com>
2025-03-26 11:12:15 +0530

    26/03 assignment

---
+++ Array/prob_5.js
@@ -0,0 +1,20 @@
+// let matrix=[
+//     [1,2,3],
+//     [4,5,6],
+//     [7,8,9]
+//   ]
+//   for(i=0;i<matrix.length;i++){
+//       let row=[]
+//       for(let j=0; j<matrix[i].length; j++){
+//           row.push(matrix[i][j])
+//       }
+//       console.log(row)
+// }
+
+
+let matrix=[
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+console.log(matrix[2][1])
---
+++ Array/shaam.js
@@ -0,0 +1,6 @@
+let arr=[1,2,3,4,5,6]
+let s=0
+for(let i=0; i<arr. length; i++){
+    s+=arr[i]
+}
+console.log(s)
